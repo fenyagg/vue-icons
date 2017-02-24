5eaa5c12f4ee4563c2ebd5a83d2a70931fbf0a52
fenyagg <fenyagg@gmail.com>
2017-02-24 04:14:49 +0300

    переписал парсинг стилей на js

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 ;{
     "use strict";
+    window.styleData = {};
+
     let iconGroupComponent = {
-        props: ['iconGroup', 'searchText'],
+        props: ['groupName', 'searchText'],
         data: function () {
             return {
-                emptyIcons: []
+                emptyIcons: [],
             }
         },
         template: `
                         <div v-if="filterIcons.length" class="c-prop-icon--icon-group">
-                            <div class="c-prop-icon--title">{{ iconGroup.NAME }}</div>
+                            <div class="c-prop-icon--title">{{ groupName }}</div>
                             <div class="c-prop-icon--list">
                                 <div
                                     v-for="(icon, index) in filterIcons"
                                     :index="index"
-                                    :key="iconGroup.NAME.toLowerCase() + '_'+index"
+                                    :key="groupName.toLowerCase() + '_'+index"
                                     class="c-prop-icon--item"
                                     >
-                                     <i :data-icon="icon" :class="[iconGroup.ICON_BASE_CLASS, icon]"></i>
+                                     <i :data-icon="icon" :class="getClass(icon)"></i>
                                 </div>
                             </div>
                         </div>`,
         computed: {
             filterIcons: function () {
-                return this.iconGroup.ICON_LIST.filter( icon => {
+                return styleData[this.groupName]['ICONS'].filter( icon => {
                     return !this.emptyIcons.includes(icon) && icon.indexOf(this.searchText) + 1;
                 });
-            }
+            },
+        },
+        methods: {
+            getClass: function(icon){
+                return [
+                    styleData[this.groupName]["ICON_BASE_CLASS"],
+                    icon
+                ];
+            },
         },
         mounted: function () {
-            //нужно чтобы найти пустые иконки и удалить их
+            //нужно найти пустые иконки и удалить их
             this.$el.querySelectorAll("i").forEach( item => {
                 if(!item.offsetHeight) this.emptyIcons.push(item.getAttribute("data-icon"));
             });
@@ -40,30 +50,43 @@
         components: {
             "icon-group": iconGroupComponent
         },
+        props: ['iconSourse'],
         template: `
                 <div class="c-prop-icon__popup">
-                    <div class="preloader" v-if="statusText">{{statusText}}</div>
+                    <div class="preloader" v-if="isLoading">Загрузка шрифтов...</div>
 
-                    <input type="text" v-model="searchText" v-if="!statusText">
-                    <template v-for="icongroup in icons">
-                        <icon-group :search-text="searchText" :icon-group="icongroup" ></icon-group>
+                    <input type="text" v-model="searchText" v-if="!isLoading">
+                    <template v-for="(groupName, index) in styleData">
+                        <icon-group :search-text="searchText" :group-name="index" ></icon-group>
                     </template>
 
                 </div>`,
         data: function(){
             return {
-                icons       : [],
+                styleData   : styleData,
                 searchText  : "",
-                statusText  : "Загрузка шрифтов..."
+                isLoading   : true,
+                errors      : []
             }
         },
         created: function () {
-            this.$http.get('response.html').then(response => {
-                this.statusText = "";
-                this.icons = response.body;
-            }, response => {
-                this.statusText = "Ошибка загрузки :(";
-            });
+            for ( let iconName in this.iconSourse) {
+                if(!styleData[iconName]) {
+                    let iconData = this.iconSourse[iconName];
+                    this.$http.get(iconData.SRC).then(response => {
+                        let icons = response.body.match(new RegExp(`(${iconData.PREFIX}[a-zA_Z_1-9-]+)`, 'ig'));
+                        if(!icons.length) this.errors.push( `В файле {iconData.SRC} иконки не найдены.`);
+                        styleData[iconName] = iconData;
+                        styleData[iconName]["ICONS"] = icons;
+                        this.isLoading = false;
+                    }, response => {
+                        this.isLoading = false;
+                        this.errors.push( `${iconData.SRC} Ошибка загрузки :(`);
+                    });
+                }
+            }
+
+
         },
     });
 };
